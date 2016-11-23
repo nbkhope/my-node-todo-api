ef4641a8aea74fdb10c4bfc9bff2308546696698
nbkhope <ryumemaru@yahoo.com>
2016-11-23 14:31:39 -0800

    Define /todos route

--- server/server.js
+++ server/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('./db/mongoose');
+const { mongoose } = require('../db/mongoose');
 
-const { Todo } = require('./models/todo');
-const { User }  = require('./models/user');
+const { Todo } = require('../models/todo');
+const { User }  = require('../models/user');
 
 const app = express();
 
 const port = 3000;
+
+// Middleware
+app.use(bodyParser.json()); // converts JSON string to JS object
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save()
+    .then((doc) => {
+      // send back new todo
+      res.send(doc);
+    }, (error) => {
+      // 400 Bad Request
+      res.status(400).send({
+        message: 'Unable to save todo',
+        error
+      });
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
