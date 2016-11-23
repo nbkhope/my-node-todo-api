83dafd009f91352fb7760967b23eefb0f2657716
nbkhope <ryumemaru@yahoo.com>
2016-11-23 14:14:43 -0800

    Add basic express server to listen on port 3000

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const { mongoose } = require('./db/mongoose');
 
 const { Todo } = require('./models/todo');
 const { User }  = require('./models/user');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
