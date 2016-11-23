2bcee637e5f255652cffd3e355dcc50a8df4e8c1
nbkhope <ryumemaru@yahoo.com>
2016-11-23 12:48:22 -0800

    Add validation to todo schema

--- server/server.js
+++ server/server.js
@@ -8,20 +8,26 @@ mongoose.connect(`${DB_URL}/MyNodeTodoApp`);
 
 const Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true // get rid of whitespace on the sides
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
     // use Number for Unix timestamp
-    type: Number
+    type: Number,
+    default: null
   }
 });
 
 // Create new todo
 const newTodo = new Todo({
-  text: 'Clean the litter box'
+  //text: 'Clean the litter box'
+  text: '    ads1d  sa   asd   '
 });
 
 // Save new todo to the DB
@@ -34,16 +40,16 @@ newTodo.save()
     console.log('Unable to save todo', error);
   });
 
-const anotherTodo = new Todo({
-  text: 'Walk the dog',
-  completed: true,
-  completedAt: 1479933477
-});
-
-anotherTodo.save()
-  .then((doc) => {
-    console.log('Saved todo.');
-    console.log(JSON.stringify(doc, undefined, 2));
-  }, (error) => {
-    console.log('Unable to save todo', error);
-  });
+// const anotherTodo = new Todo({
+//   text: 'Walk the dog',
+//   completed: true,
+//   completedAt: 1479933477
+// });
+//
+// anotherTodo.save()
+//   .then((doc) => {
+//     console.log('Saved todo.');
+//     console.log(JSON.stringify(doc, undefined, 2));
+//   }, (error) => {
+//     console.log('Unable to save todo', error);
+//   });
