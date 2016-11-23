3fa83fe3092f1f156c693575286795df7f38c19b
nbkhope <ryumemaru@yahoo.com>
2016-11-23 14:38:52 -0800

    Move db/ and models/ directories into server/ and update server.js to import correct paths

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoose } = require('../db/mongoose');
+const { mongoose } = require('./db/mongoose');
 
-const { Todo } = require('../models/todo');
-const { User }  = require('../models/user');
+const { Todo } = require('./models/todo');
+const { User }  = require('./models/user');
 
 const app = express();
 
