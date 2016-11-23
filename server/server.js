32ce859a544a35818dffa7c51e1db6ef390dfcc2
nbkhope <ryumemaru@yahoo.com>
2016-11-23 14:35:59 -0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,13 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((docs) => {
+      res.send(docs);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
