820fd0f9a30b6996013d3764e116654e5a8d373d
nbkhope <ryumemaru@yahoo.com>
2016-11-24 12:23:02 -0800

    Add test for POST /todos route where given todo is invalid

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -41,6 +41,24 @@ describe('POST /todos', () => {
           });
       });
   });
+
+  it('should not create a todo with invalid body data', (done) => {
+    request(app)
+      .post('/todos')
+      .send({})
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        // Make sure no todo was saved to the DB
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((e) => done(e));
+      });
+  });
 });
 
 describe ('GET /todos', () => {
