535d77c4fd8e268dda530854e26a81f669a5fee3
nbkhope <ryumemaru@yahoo.com>
2016-12-05 16:00:55 -0800

    Add process.env.MONGODB_URI in db connection to MongoDB

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // Configure mongoose to use JavaScript Promise
 mongoose.Promise = global.Promise;
 // Connect to the database
-const DB_URL = 'mongodb://localhost:27017';
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 mongoose.connect(`${DB_URL}/MyNodeTodoApp`);
 
 module.exports = {
