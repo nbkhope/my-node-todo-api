74579cf63bbadaffeaceab85e0d326445c3914dd
nbkhope <ryumemaru@yahoo.com>
2016-12-07 15:54:30 -0800

    Add configuration to use a test database instead of the development one

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 // Configure mongoose to use JavaScript Promise
 mongoose.Promise = global.Promise;
 // Connect to the database
-const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017';
-mongoose.connect(`${DB_URL}/MyNodeTodoApp`);
+const DB_URL = process.env.MONGODB_URI;
+mongoose.connect(DB_URL);
 
 module.exports = {
   mongoose
